b662f997c49f1d802d54a837d87f02a9684b6d6a
bajajmanoj <manoj@falconx.io>
2021-09-15 18:36:06 +0000

    Updated zsh history script/emacs setup guide

--- Scripts/shrink-zsh-history.py
+++ Scripts/shrink-zsh-history.py
@@ -3,18 +3,21 @@ historyCache={}
 
 def loadFile(filename):
     with open(filename,'r') as f:
-        for line in f:
+        buffer = f.read()
+        commands = buffer.split("\n:")[1:] # \n: is used as separator for each history command
+        for command in commands:
             try:
-                cmd = line.split(";")[1]
-                historyCache[cmd]=line
+                cmd = command.split(";")[1]
+                historyCache[cmd]=command
             except :
                 #  This is probably multiline command which is yet to be handled
-                print(line)
+                print(command)
+            # break
 
 def writeFile(filename):
     with open(filename,'w') as f:
         for k,v in historyCache.items():
-            f.write(v)
+            f.write(": " + v + "\n")
 
 if __name__ == "__main__":
     histFile = sys.argv[1]
